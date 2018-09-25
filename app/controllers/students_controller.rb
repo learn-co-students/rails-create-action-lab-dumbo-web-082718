40d8f98fa5a83f025ad9a26393569968f5be6782
github username <anthony.s.lee96@gmail.com>
2018-09-25 15:01:31 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,10 +8,34 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+  
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student).permit(:first_name, :last_name))
+      redirect_to student_path(@student)
+
+  end
+
   def new
   end
 
+  def destroy
+    @student = Student.find(params[:id]).destroy
+    redirect_to students_path
+  end
+
   def create
+    @student = Student.create(
+    {
+      first_name: params[:first_name],
+      last_name:  params[:last_name]
+    }
+    )
+    
+    redirect_to student_path(@student)
   end
 
 end
